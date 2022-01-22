aa0b3db17234af7ac9cc710dd6d92c6b58a28108
ckruger097 <ckruger4@gmu.edu>
2022-01-21 20:05:54 -0500

    removed redundant remove_collateral_event.ts

---
+++ src/add_remove_collateral_event.ts
@@ -0,0 +1,78 @@
+import BigNumber from "bignumber.js";
+import {
+    Finding,
+    FindingSeverity,
+    FindingType,
+    HandleTransaction,
+    TransactionEvent
+} from "forta-agent";
+
+import {LOGADDCOLLATERAL_EVENT, ETH_DECIMALS, LOGREMOVECOLLATERAL_EVENT} from "./constants";
+
+function providerEventTransaction(
+    cauldronMap: Map<string,string>
+): HandleTransaction {
+    return async function handleTransaction(txEvent: TransactionEvent) {
+        const findings: Finding[] = [];
+
+        for (let entry of Array.from(cauldronMap.entries())) {
+            let cauldronAddress = entry[0];
+            let cauldronName = entry[1];
+            const collateralEvent = txEvent.filterLog([
+                LOGADDCOLLATERAL_EVENT,
+                LOGREMOVECOLLATERAL_EVENT
+                ],
+                cauldronAddress,
+            );
+            if (!collateralEvent.length) continue
+
+
+            collateralEvent.forEach((collateralEvent) => {
+                const sharesTransferred = new BigNumber(
+                    collateralEvent.args.share.toString()
+                ).dividedBy(10 ** ETH_DECIMALS);
+                const formattedAmount = sharesTransferred.toFixed(2);
+
+                if (collateralEvent.name == "LogAddCollateral") {
+                    findings.push(
+                        Finding.fromObject({
+                            name: `${collateralEvent.name} Event in ${cauldronName} Cauldron`,
+                            description: `${formattedAmount} shares ${cauldronName} added`,
+                            alertId: "ABRA-1",
+                            severity: FindingSeverity.Info,
+                            type: FindingType.Info,
+                            metadata: {
+                                from: collateralEvent.args.from.toString(),
+                                to: collateralEvent.args.to.toString(),
+                                share: collateralEvent.args.share.toString(),
+                            },
+                        })
+                    );
+                }
+                if (collateralEvent.name == "LogRemoveCollateral") {
+                    findings.push(
+                        Finding.fromObject({
+                            name: `LogRemoveCollateral Event in ${cauldronName} Cauldron`,
+                            description: `${formattedAmount} shares ${cauldronName} removed`,
+                            alertId: "ABRA-2",
+                            severity: FindingSeverity.Info,
+                            type: FindingType.Info,
+                            metadata: {
+                                from: collateralEvent.args.from.toString(),
+                                to: collateralEvent.args.to.toString(),
+                                share: collateralEvent.args.share.toString(),
+                            },
+                        })
+                    );
+                }
+
+
+            });
+        }
+        return findings;
+    };
+}
+
+export default {
+    providerEventTransaction: providerEventTransaction,
+}
--- src/agent.ts
+++ src/agent.ts
@@ -1,17 +1,13 @@
 import { TransactionEvent } from 'forta-agent'
-import addCollateralEvents from './add_collateral_event'
-import removeCollateralEvents from './remove_collateral_event'
+import addCollateralEvents from './add_remove_collateral_event'
 import { CAULDRON_ADDRESS_MAP } from './constants'
 
 export const provideHandleTransaction = (cauldronMap: Map<string, string>) => {
 
   return async function handleTransaction(txEvent: TransactionEvent) {
-    // Can use one or both event handlers
     const addCollateralHandler = addCollateralEvents.providerEventTransaction(cauldronMap)
-    const removeCollateralHandler = removeCollateralEvents.providerEventTransaction(cauldronMap)
-    const addCollateralEventFindings = await addCollateralHandler(txEvent)
-    const removeCollateralEventFindings = await removeCollateralHandler(txEvent)
-    return addCollateralEventFindings.concat(removeCollateralEventFindings)
+    const findings = await addCollateralHandler(txEvent)
+    return findings
   }
 }
 
